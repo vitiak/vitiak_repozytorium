9135047a22042fcf853a453d9bc2f76940c18ab9
vitiak <kwaswi@wp.pl>
2017-07-28 19:31:41 +0200

    First Commit

---
+++ src/main/java/com/codilla/Test1Class.java
@@ -0,0 +1,29 @@
+package com.codilla;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class SimpleCalculator
+{
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+}
+
+class Application
+{
+    public static void main(String[] args) {
+
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        int result = simpleCalculator.addAToB(5, 5);
+
+        System.out.println(result);
+
+    }
+}
