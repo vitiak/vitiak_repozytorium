7496e84255f1f069db8dd0ad1920de6d0b1dca0b
vitiak <kwaswi@wp.pl>
2017-08-04 23:09:16 +0200

    4Com

---
+++ src/main/java/com/codilla/testing/TestingMain.java
@@ -0,0 +1,8 @@
+package com.codilla.testing;
+
+public class TestingMain {
+
+    public static void main(String[] args){
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    }
+}
