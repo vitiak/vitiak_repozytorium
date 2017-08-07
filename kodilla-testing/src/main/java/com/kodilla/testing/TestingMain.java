5f28f6d5bb0a9432602522c3d06077e21378cc99
vitiak <kwaswi@wp.pl>
2017-08-07 19:11:31 +0200

    5Comm_Modul_6_2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.addAToB(2,3);
+        if (resultAdd == 2+3) {
+            System.out.println("test addAToB OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int resultSubstract = calculator.substractAFromB(2,3);
+        if (resultSubstract == 3-2) {
+            System.out.println("test substractAFromB OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("TO O TO CHODZILO???");
     }
 }
