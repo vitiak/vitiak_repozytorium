90d2729c29dda963bca36fca8bc89cf93b9d72d4
vitiak <kwaswi@wp.pl>
2017-08-07 20:26:22 +0200

    6Comm_Modul_6_2cd

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/simpleUser.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing;
+
+public class simpleUser {
+    String username;
+    public SimpleUser(String username){
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+}
