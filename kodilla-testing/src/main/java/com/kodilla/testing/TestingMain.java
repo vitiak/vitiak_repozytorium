8e4fdce7e1e58981470f2c793c4179f413c79e21
vitiak <kwaswi@wp.pl>
2017-08-08 18:23:01 +0200

    7Comm_Modul_6_3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,25 +1,8 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
 
     public static void main(String[] args){
-        Calculator calculator = new Calculator();
-
-        int resultAdd = calculator.addAToB(2,3);
-        if (resultAdd == 2+3) {
-            System.out.println("test addAToB OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        int resultSubstract = calculator.substractAFromB(2,3);
-        if (resultSubstract == 3-2) {
-            System.out.println("test substractAFromB OK");
-        } else {
-            System.out.println("Error!");
-        }
-        System.out.println("TO O TO CHODZILO???");
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/simpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/simpleUser.java
@@ -8,4 +8,5 @@ public class simpleUser {
     public String getUsername() {
         return username;
     }
+
 }
