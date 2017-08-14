0874810fda39ce4197d7500511231ebfa164e27d
vitiak <kwaswi@wp.pl>
2017-08-14 14:28:30 +0200

    12Commit modul 6.4 blad, ale czemu?

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/tdd/ForumTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.testing.forum.tdd;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.*;
+
+public class ForumTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testAddPost() {
+        //Given
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+
+        //When
+        forumUser.addPost("mrsmith",
+                "Hello everyone, this is my first contribution here!");
+
+        //Then
+        Assert.assertEquals(1, forumUser.getPostsQuantity());
+    }
+
+}
