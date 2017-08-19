7fb2792c41c1fac6e2d7047610c38beab3f858cf
vitiak <kwaswi@wp.pl>
2017-08-19 22:56:46 +0200

    22 Commit modul 7.3 zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -10,7 +10,7 @@ public class StreamMain {
     public static void main(String[] args) {
         Forum theForum = new Forum();
         Map<Integer, ForumUser> theResultStringOfForumUser = theForum.getUserList().stream()
-                .filter(forumUser -> forumUser.getSex() = 'M')
+                .filter(forumUser -> forumUser.getSex() == 'M')
                 .filter(forumUser -> forumUser.getDateOfBirth().getYear() < 1998)
                 .filter(forumUser -> forumUser.getCountOfPosts() > 0)
                 .collect(Collectors.toMap(ForumUser::getPersonId, forumUser->forumUser));
