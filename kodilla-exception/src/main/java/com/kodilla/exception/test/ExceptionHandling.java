694e3b74787f5b13030bd40ef95fdb0640b7b396
vitiak <kwaswi@wp.pl>
2017-09-02 15:26:12 +0200

    32 Commit zadanie 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -4,15 +4,17 @@ public class ExceptionHandling extends Exception {
 
     /*  NIE MAM POJĘCIA O CO CHODZI W TYM ZADANIU, tzn. co ja mam zrobić  */
 
-    SecondChalenge secondChalenge = new SecondChalenge();
 
-    try {
-        String result = secondChalenge.probablyIWillThrowException(2.4, 3.5);
-        System.out.println(result);
-    } catch (ArithmeticException e) {
-        System.out.println("Wystąpił błąd przy porównywaniu! " + e);
-    } finally {
-        System.out.println("Koniec programu!");
+    public static void main(String[] args) throws Exception {
+        SecondChalenge secondChalenge = new SecondChalenge();
+
+        try {
+            String result = secondChalenge.probablyIWillThrowException(2.4, 3.5);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Wystąpił błąd przy porównywaniu! " + e);
+        } finally {
+            System.out.println("Koniec programu!");
+        }
     }
-
 }
