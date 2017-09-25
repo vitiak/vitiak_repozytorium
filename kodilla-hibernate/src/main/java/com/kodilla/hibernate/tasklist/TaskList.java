896290decc9682b6ab756dff8e2ecb652dc3b6d6
vitiak <kwaswi@wp.pl>
2017-09-25 18:57:45 +0200

    58 Commit zadanie 13.3 blad

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,7 +1,11 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
@@ -9,6 +13,7 @@ public class TaskList {
     private int id;
     private String description;
     private String listName;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
@@ -35,6 +40,20 @@ public class TaskList {
     @Column(name="LISTNAME")
     public String getListName() {
         return listName;
+    }
+
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     private void setId(int id) {
