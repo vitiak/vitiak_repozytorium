cdc90752f133adc6253afb0aa7f9b1d2a79136d0
vitiak <kwaswi@wp.pl>
2017-12-11 18:45:22 +0100

    zadanie 23.3

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,40 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+
+    public static final String XPATH_WAIT_FOR1 = "//select[1]";
+    public static final String XPATH_WAIT_FOR2 = "//select[2]";
+    public static final String XPATH_WAIT_FOR3 = "//select[3]";
+    public static final String XPATH_SELECT1 = "//div[contains(@class, \"_58mq _5dbb\")]/div/span/span/select[1]";
+    public static final String XPATH_SELECT2 = "//div[contains(@class, \"_58mq _5dbb\")]/div/span/span/select[2]";
+    public static final String XPATH_SELECT3 = "//div[contains(@class, \"_58mq _5dbb\")]/div/span/span/select[3]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.facebook.com/");
+
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR1)).isDisplayed());
+
+        WebElement selectCombo1 = driver.findElement(By.xpath(XPATH_SELECT1));
+        Select selectBoard = new Select(selectCombo1);
+        selectBoard.selectByIndex(20);
+
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR2)).isDisplayed());
+
+        WebElement selectCombo2 = driver.findElement(By.xpath(XPATH_SELECT2));
+        Select selectBoard2 = new Select(selectCombo2);
+        selectBoard2.selectByIndex(8);
+
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR3)).isDisplayed());
+
+        WebElement selectCombo3 = driver.findElement(By.xpath(XPATH_SELECT3));
+        Select selectBoard3 = new Select(selectCombo3);
+        selectBoard3.selectByIndex(22);
+    }
+}
